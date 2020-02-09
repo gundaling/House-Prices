7c54ee4103bbff870d17657283d1ca6dba789df8
bulu <angga053@yahoo.com>
2020-02-09 20:58:39 +0700

    revisi 2020-02-01

--- House-Prices-Analysis.R
+++ House-Prices-Analysis.R
@@ -15,8 +15,9 @@ test_house <- test_house %>%
 df_full <- train_house %>% 
   bind_rows(test_house)
 
-df_full<- df_full %>% 
-  mutate_if(is.factor, as.character)
+df_full <- df_full %>% 
+  mutate(YearRemodAdd = if_else(YearRemodAdd < YearBuilt, YearBuilt, YearRemodAdd),
+         GarageYrBlt = if_else((!is.na(GarageYrBlt) & GarageYrBlt < YearBuilt), YearBuilt, GarageYrBlt))
 
 df_full<- df_full %>% 
   mutate(Remodel = if_else(YearBuilt == YearRemodAdd, 'Y', 'N'))
@@ -25,6 +26,8 @@ df_full<- df_full %>%
   mutate(MSSubClass = as.factor(MSSubClass),
          OverallQual = as.factor(OverallQual),
          OverallCond = as.factor(OverallCond))
+
+
 
 df_full <- df_full %>% 
   mutate(HouseAgeOld = YrSold - YearBuilt,
@@ -53,42 +56,96 @@ df_full %>%
 
 df_full$MasVnrArea[df_full$MasVnrType == 'None'] <- 0
 
-## BsmtQual ===
+df_full %>% filter(is.na(MasVnrArea)| is.na(MasVnrType)) %>% select(MasVnrType, MasVnrArea) %>% view()
+## Basement ===
 
-df_full$BsmtQual[df_full$BsmtQual == 'None'] <- NA
+(df_full %>% 
+  select(contains('Bsmt')))[!complete.cases(df_full %>% 
+                                              select(contains('Bsmt'))),] %>% view()
 
-## BsmtQual ===
+df_full <- df_full %>%
+  mutate(BsmtQual = if_else((BsmtFinSF1 == 0 & BsmtUnfSF == 0), 'None', BsmtQual),
+         BsmtCond = if_else((BsmtFinSF1 == 0 & BsmtUnfSF == 0), 'None', BsmtCond),
+         BsmtExposure = if_else((BsmtFinSF1 == 0 & BsmtUnfSF == 0), 'None', BsmtExposure),
+         BsmtFinType1 = if_else((BsmtFinSF1 == 0 & BsmtUnfSF == 0), 'None', BsmtFinType1),
+         BsmtFinType2 = if_else((BsmtFinSF2 == 0 & BsmtUnfSF == 0), 'None', BsmtFinType2),
+         BsmtFullBath = if_else((BsmtFinType1 == 'None' & BsmtFinType2 == 'None'), 0, BsmtFullBath),
+         BsmtHalfBath = if_else((BsmtFinType1 == 'None' & BsmtFinType2 == 'None'), 0, BsmtHalfBath))
 
-df_full$BsmtQual[is.na(df_full$BsmtQual)] <- 'None'
+df_full <- df_full %>%
+  mutate(BsmtQual = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 'None', BsmtQual),
+         BsmtCond = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 'None', BsmtCond),
+         BsmtExposure = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 'None', BsmtExposure),
+         BsmtFinType1 = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 'None', BsmtFinType1),
+         BsmtFinSF1 = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 0, BsmtFinSF1),
+         BsmtFinType2 = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 'None', BsmtFinType2),
+         BsmtFinSF2 = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 0, BsmtFinSF2),
+         BsmtUnfSF = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 0, BsmtUnfSF),
+         BsmtFullBath = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 0, BsmtFullBath),
+         BsmtHalfBath = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 0, BsmtHalfBath),
+         TotalBsmtSF = if_else((is.na(BsmtFinType1) & is.na(BsmtFinType2)), 0, TotalBsmtSF))
 
-## BsmtQual ===
-
-df_full$BsmtQual[is.na(df_full$BsmtQual)] <- 'None'
-
-df_full %>% 
-  filter(is.na(BsmtQual1)|
-         is.na(BsmtCond1)|
-         is.na(BsmtExposure1)) %>% 
-  select(BsmtQual1, BsmtCond1, BsmtExposure1) %>% view()
-
-df_full <- df_full %>% 
-  mutate(BsmtQual1 = if_else((is.na(BsmtQual)&is.na(BsmtCond)&is.na(BsmtExposure)), 'None', BsmtQual),
-         BsmtCond1 = if_else((is.na(BsmtQual)&is.na(BsmtCond)&is.na(BsmtExposure)), 'None', BsmtCond),
-         BsmtExposure1 = if_else((is.na(BsmtQual)&is.na(BsmtCond)&is.na(BsmtExposure)), 'None', BsmtExposure))
-
-df_full %>% 
-  count(BsmtQual1, BsmtCond1, BsmtExposure1) %>% 
-  filter(BsmtQual)
+df_full <- df_full %>%
+  mutate(BsmtFinSF1 = if_else(BsmtFinType1 == 'None', 0, BsmtFinSF1),
+         BsmtFinType2 = if_else(BsmtFinType1 == 'None', 'None', BsmtFinType2),
+         BsmtFinSF2 = if_else(BsmtFinType1 == 'None', 0, BsmtFinSF2),
+         BsmtUnfSF = if_else(BsmtFinType1 == 'None', 0, BsmtUnfSF),
+         BsmtFullBath = if_else(BsmtFinType1 == 'None', 0, BsmtFullBath),
+         BsmtHalfBath = if_else(BsmtFinType1 == 'None', 0, BsmtHalfBath),
+         TotalBsmtSF = if_else(BsmtFinType1 == 'None', 0, TotalBsmtSF))
 
 df_bsmt <- df_full %>% 
-  select(BsmtQual1, BsmtCond1, BsmtExposure1) %>% 
+  select(contains('Bsmt')) %>% 
   mutate_if(is.character, as.factor)
-  
+
 TrainDummBsmt <- dummyVars(~., df_bsmt)
 df_bsmt_dumm <- predict(TrainDummBsmt, df_bsmt)
 
 preprocess_bsmt <- preProcess(df_bsmt_dumm, method = 'bagImpute')
 df_bsmt_imputed <- predict(preprocess_bsmt, df_bsmt_dumm)
+
+## Fireplace ===
+
+df_full %>% 
+  filter(is.na(FireplaceQu)) %>% 
+  select(Fireplaces, FireplaceQu) %>% count(Fireplaces)
+
+df_full <- df_full %>% 
+  mutate(FireplaceQu = if_else(Fireplaces == 0, 'None', FireplaceQu))
+
+## Garage ===
+
+(df_full %>% 
+    select(contains('Garage')))[!complete.cases(df_full %>% 
+                                                select(contains('Garage'))),] %>% view()
+
+df_full <- df_full %>%
+  mutate(GarageType = if_else((GarageCars == 0 | GarageArea == 0), 'None', GarageType),
+         GarageYrBlt = if_else((GarageCars == 0 | GarageArea == 0), 0, GarageYrBlt),
+         GarageFinish = if_else((GarageCars == 0 | GarageArea == 0), 'None', GarageFinish),
+         GarageCars = if_else((GarageCars == 0 | GarageArea == 0), 0, GarageCars),
+         GarageArea = if_else((GarageCars == 0 | GarageArea == 0), 0, GarageArea),
+         GarageQual = if_else((GarageCars == 0 | GarageArea == 0), 'None', GarageQual),
+         GarageCond = if_else((GarageCars == 0 | GarageArea == 0), 'None', GarageCond))
+
+df_full <- df_full %>%
+  mutate(GarageType = if_else(is.na(GarageArea), 'None', GarageType),
+         GarageYrBlt = if_else(is.na(GarageArea), 0, GarageYrBlt),
+         GarageFinish = if_else(is.na(GarageArea), 'None', GarageFinish),
+         GarageCars = if_else(is.na(GarageArea), 0, GarageCars),
+         GarageArea = if_else(is.na(GarageArea), 0, GarageArea),
+         GarageQual = if_else(is.na(GarageArea), 'None', GarageQual),
+         GarageCond = if_else(is.na(GarageArea), 'None', GarageCond))
+
+df_full %>% 
+  filter(is.na(GarageYrBlt)|gar) %>% 
+  select(contains('Garage'), YearBuilt, YearRemodAdd)
+
+## Kitchen
+
+df_full %>% 
+  select(contains('Kitchen'))
+
 
 # Explore -----------------------------------------------------------------
 
